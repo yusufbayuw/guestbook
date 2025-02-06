4330d265f271fdd4b448f18b1e59cfa5c92934cf
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-02-06 11:27:56 +0700

    tab guest

--- app/Filament/Resources/GuestResource/Pages/ManageGuests.php
+++ app/Filament/Resources/GuestResource/Pages/ManageGuests.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\GuestResource\Pages;
 
-use App\Filament\Resources\GuestResource;
 use Filament\Actions;
+use App\Models\Gedung;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\GuestResource;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageGuests extends ManageRecords
@@ -15,5 +17,30 @@ class ManageGuests extends ManageRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = ['semua' => Tab::make('semua')->badge($this->getModel()::whereNull('pulang')->count())];
+
+        //$tabs = [];
+
+        $gedungs = Gedung::withCount('guest')
+            ->has('guest')
+            ->get();
+
+        foreach ($gedungs as $gedung) {
+            $name = $gedung->nama;
+            $slug = str($name)->slug()->toString();
+
+            $tabs[$slug] = Tab::make($name)
+                ->badge($this->getModel()::whereNull('pulang')->where('gedung_id', $gedung->id)->count())
+                ->badgeColor("primary")
+                ->modifyQueryUsing(function ($query) use ($gedung) {
+                    return $query->where('gedung_id', $gedung->id);
+                });
+        }
+
+        return $tabs;
     }
 }
